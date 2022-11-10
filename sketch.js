50e9a766015d5a4f95e10aed8794dbc2235b2c8f
arthurto <arthur.ps22009@gmail.com>
2022-11-09 21:16:58 -0300

    test

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var t = 0
-
+var m = 1
 function setup() {
   createCanvas(400, 400);
 }
@@ -7,7 +7,13 @@ function setup() {
 function draw() {
   background(220);
   
-  circle(20,20,t,t)
+  circle(200,200,t,t)
   
-  t++
+  t = t + m*5
+  
+  if (t > 500 && m == 1) {
+    m = -1 
+  } else if (t < 0){
+    m = 1 
+  }
 }
